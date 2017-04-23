ae20cb21943954fd5723a219d952100c176d983b
R1gp4 <Roman@Romans-MacBook-Air.local>
2017-04-23 22:09:05 +0200

    changed the loop

--- hwautoscript.py
+++ hwautoscript.py
@@ -3,14 +3,14 @@ import time
 
 pfd = pifacedigitalio.PiFaceDigital() 
 
-pfd.relays[0].value = 1
-pfd.relays[1].value = 1
-
 while True:
     print("This prints once a minute.")
-time.sleep(60)  # Delay for 1 minute (60 seconds)
-
-pfd.relays[0].value = 0
-pfd.relays[1].value = 0
+	time.sleep(60)  # Delay for 1 minute (60 seconds)
+	pfd.relays[0].value = 1
+	pfd.relays[1].value = 1
+	time.sleep(60) 
+	print("now the relays will turn off")
+	pfd.relays[0].value = 0
+	pfd.relays[1].value = 0
 
 
