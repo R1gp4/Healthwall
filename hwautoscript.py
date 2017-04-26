37ba7cdc4a3a7f745cdb849549c0bb786224b936
R1gp4 <Roman@Romans-MacBook-Air.local>
2017-04-26 18:12:15 +0200

    parenthesis and text

--- hwautoscript.py
+++ hwautoscript.py
@@ -3,12 +3,12 @@ import time
 pfd = pifacedigitalio.PiFaceDigital() 
 
 while True:
-	print("The relays are now ON" 
+	print("The relays are now ON")
 	pfd.relays[0].value = 1 # lights on
 	pfd.relays[1].value = 1 # pump on
 	time.sleep(60) # keep this setting for one minute (60 seconds)
 	
-	print("now the relays will turn off")
+	print("The relays are now OFF")
 	pfd.relays[0].value = 0 # lights off
 	pfd.relays[1].value = 0 # pump off
 	time.sleep(86400) # keep this setting for one minute (60 seconds)
