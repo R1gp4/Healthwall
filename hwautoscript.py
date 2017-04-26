49f16e4e4047012d3c0fa7eed9ba6b7bff4a8c1b
R1gp4 <Roman@Romans-MacBook-Air.local>
2017-04-26 18:08:07 +0200

    new order of events

--- hwautoscript.py
+++ hwautoscript.py
@@ -1,16 +1,22 @@
 import pifacedigitalio
 import time 
-
 pfd = pifacedigitalio.PiFaceDigital() 
 
 while True:
-	print("This prints once a minute.")
-	time.sleep(60)  # Delay for 1 minute (60 seconds)
-	pfd.relays[0].value = 1
-	pfd.relays[1].value = 1
-	time.sleep(60) 
+	print("The relays are now ON" 
+	pfd.relays[0].value = 1 # lights on
+	pfd.relays[1].value = 1 # pump on
+	time.sleep(60) # keep this setting for one minute (60 seconds)
+	
 	print("now the relays will turn off")
-	pfd.relays[0].value = 0
-	pfd.relays[1].value = 0
+	pfd.relays[0].value = 0 # lights off
+	pfd.relays[1].value = 0 # pump off
+	time.sleep(86400) # keep this setting for one minute (60 seconds)
+
+
+
+
+
+
 
 
