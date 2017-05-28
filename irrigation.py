91f3a0f1083e6038bffdcf72d2b7cf6884ff24eb
Roman Korver <roman.korver@gmail.com>
2017-05-28 14:06:49 +0200

    improvement on shortness of irrigating

--- irrigation.py
+++ irrigation.py
@@ -6,5 +6,12 @@ import time
 pfd = pifacedigitalio.PiFaceDigital() 
 
 pfd.relays[1].value = 1 # pump on
+time.sleep(15) # keep this setting for () seconds
+print("irrigating for 15 seconds...")
+pfd.relays[1].value = 0 # pump off
 time.sleep(30) # keep this setting for () seconds
+print("waiting 30 seconds...")
+pfd.relays[1].value = 1 # pump on
+time.sleep(15) # keep this setting for () seconds
+print("irrigating for 15 seconds...")
 pfd.relays[1].value = 0 # pump off
