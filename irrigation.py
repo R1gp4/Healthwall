5a0f90bc303adfeea16f7d0b424f0f66d347cceb
Roman Korver <roman.korver@gmail.com>
2017-05-28 14:23:27 +0200

    longer irrigation

--- irrigation.py
+++ irrigation.py
@@ -3,15 +3,15 @@ Irrigation
 '''
 import pifacedigitalio
 import time 
-pfd = pifacedigitalio.PiFaceDigital() 
+pfd = pifacedigitalio.PiFaceDigital()
 
 pfd.relays[1].value = 1 # pump on
-time.sleep(15) # keep this setting for () seconds
-print("irrigating for 15 seconds...")
+time.sleep(30) # keep this setting for () seconds
+print("irrigating for 30 seconds...")
 pfd.relays[1].value = 0 # pump off
 time.sleep(30) # keep this setting for () seconds
 print("waiting 30 seconds...")
 pfd.relays[1].value = 1 # pump on
-time.sleep(15) # keep this setting for () seconds
-print("irrigating for 15 seconds...")
+time.sleep(30) # keep this setting for () seconds
+print("irrigating for 30 seconds...")
 pfd.relays[1].value = 0 # pump off
