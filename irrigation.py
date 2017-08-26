9257180cfb8d9e6ad5e99fb0992b0ba7a775021e
Roman Korver <roman.korver@gmail.com>
2017-08-26 18:04:27 +0200

    changed irrigation time to 40 sec

--- irrigation.py
+++ irrigation.py
@@ -7,7 +7,7 @@ pfd = pifacedigitalio.PiFaceDigital()
 
 pfd.relays[1].value = 1 # pump on
 time.sleep(40) # keep this setting for () seconds
-print("irrigating for 30 seconds...")
+print("irrigating for 40 seconds...")
 pfd.relays[1].value = 0 # pump off
 
 
