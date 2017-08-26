c49b8bba8c0238e62f0bc40e0474dcc9e3701bdd
Roman Korver <roman.korver@gmail.com>
2017-08-26 18:02:35 +0200

    improvement on irrigation length and elimination on interruption

--- irrigation.py
+++ irrigation.py
@@ -1,9 +1,22 @@
-'''
-Irrigation
-'''
+
+''' Irrigation '''
+
 import pifacedigitalio
 import time 
 pfd = pifacedigitalio.PiFaceDigital()
+
+pfd.relays[1].value = 1 # pump on
+time.sleep(40) # keep this setting for () seconds
+print("irrigating for 30 seconds...")
+pfd.relays[1].value = 0 # pump off
+
+
+"""
+''' Nothing '''
+time.sleep(3600*12)
+
+
+''' Irrigation '''
 
 pfd.relays[1].value = 1 # pump on
 time.sleep(30) # keep this setting for () seconds
@@ -15,3 +28,5 @@ pfd.relays[1].value = 1 # pump on
 time.sleep(30) # keep this setting for () seconds
 print("irrigating for 30 seconds...")
 pfd.relays[1].value = 0 # pump off
+
+"""
