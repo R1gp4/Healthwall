4adc4eb1ac212c8cefc8da3e6d71c0c7e8f156b8
Roman Korver <roman.korver@gmail.com>
2018-08-15 20:47:53 +0200

    text while executing

--- irrigation.py
+++ irrigation.py
@@ -2,13 +2,13 @@
 ''' Irrigation '''
 #setup
 import pifacedigitalio
-import time 
+import time
 pfd = pifacedigitalio.PiFaceDigital()
 
 #main
 try:
     pfd.relays[1].value = 1 # pump on
-    print("irrigating for 40 seconds...")
+    print("irrigating for 40 seconds... press ctrl+c to interrupt")
     time.sleep(40) # keep this setting for () seconds
     pfd.relays[1].value = 0 # pump off
 except (KeyboardInterrupt, SystemExit):
