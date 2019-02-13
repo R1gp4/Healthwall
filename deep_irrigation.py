93a6ea3934885525e33471fa4a82fe347b9711f8
Roman Korver <roman.korver@gmail.com>
2019-02-13 18:13:28 +0100

    sleepTime variable

--- deep_irrigation.py
+++ deep_irrigation.py
@@ -4,13 +4,16 @@
 import pifacedigitalio
 import time
 pfd = pifacedigitalio.PiFaceDigital()
+sleepTime = 240
 
 #main
 try:
     pfd.relays[1].value = 1 # pump on
-    print("irrigating for 40 seconds... press ctrl+c to interrupt")
-    time.sleep(240) # keep this setting for (240) seconds
+    pfd.relays[0].value = 1 # lights on
+    print("irrigating for " + str(sleepTime) + " seconds... press ctrl+c to interrupt")
+    time.sleep(sleepTime) # keep this setting for (240) seconds
     pfd.relays[1].value = 0 # pump off
+
 except (KeyboardInterrupt, SystemExit):
     pfd.relays[1].value = 0 #if something goes wrong, turn off the pump
     print("Interrupted by user")
