a6812d00e9b4176684cd26ef308b6dcffed17aef
Roman Korver <roman.korver@gmail.com>
2019-08-20 22:55:41 +0200

    added back sleep length

--- irrigation.py
+++ irrigation.py
@@ -2,10 +2,9 @@
 import pifacedigitalio
 import time
 pfd = pifacedigitalio.PiFaceDigital()
-
+sleepTime = 240
 
 #main
-<<<<<<< HEAD
 # def irrigation(sleepTime):
 ''' Irrigation script '''
 try:
@@ -20,19 +19,3 @@ except (KeyboardInterrupt, SystemExit):
     print("Interrupted by user")
 
 # irrigation(200)
-=======
-def irrigation(sleepTime):
-    ''' Irrigation script '''
-    try:
-        pfd.relays[0].value = 1 # lights on
-        pfd.relays[1].value = 1 # pump on
-        print("irrigating for " + str(sleepTime) + " seconds... press ctrl+c to interrupt")
-        time.sleep(sleepTime) # keep this setting for () seconds
-        pfd.relays[1].value = 0 # pump off
-
-    except (KeyboardInterrupt, SystemExit):
-        pfd.relays[1].value = 0 #if something goes wrong, turn off the pump
-        print("Interrupted by user")
-
-irrigation(200)
->>>>>>> fc5ab115b9d21afd0ec94adb5b65f867d72c0274
