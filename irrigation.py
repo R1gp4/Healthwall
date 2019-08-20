fc5ab115b9d21afd0ec94adb5b65f867d72c0274
Roman Korver <roman.korver@gmail.com>
2019-08-20 22:31:06 +0200

    refactored it into a function

--- irrigation.py
+++ irrigation.py
@@ -1,20 +1,21 @@
-
-''' Irrigation '''
 #setup
 import pifacedigitalio
 import time
 pfd = pifacedigitalio.PiFaceDigital()
-sleepTime = 240
+
 
 #main
-try:
-    pfd.relays[0].value = 1 # lights on
-    pfd.relays[1].value = 1 # pump on
-    print("irrigating for " + str(sleepTime) + " seconds... press ctrl+c to interrupt")
-    time.sleep(sleepTime) # keep this setting for () seconds
-    pfd.relays[1].value = 0 # pump off
+def irrigation(sleepTime):
+    ''' Irrigation script '''
+    try:
+        pfd.relays[0].value = 1 # lights on
+        pfd.relays[1].value = 1 # pump on
+        print("irrigating for " + str(sleepTime) + " seconds... press ctrl+c to interrupt")
+        time.sleep(sleepTime) # keep this setting for () seconds
+        pfd.relays[1].value = 0 # pump off
 
-except (KeyboardInterrupt, SystemExit):
-    pfd.relays[1].value = 0 #if something goes wrong, turn off the pump
-    print("Interrupted by user")
-    
+    except (KeyboardInterrupt, SystemExit):
+        pfd.relays[1].value = 0 #if something goes wrong, turn off the pump
+        print("Interrupted by user")
+
+irrigation(200)
